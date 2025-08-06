7aebb798308e971d07be4fa6113a010df3c88073
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-08-06 14:57:57 +0700

    z

--- api/token.js
+++ api/token.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 
 export function token() {
-  const url = 'https://moph-rbac.id.th/api/v1/token?Action=get_moph_access_token&user=system_dev&password_hash=AD1F08EF0CA9CC007696D1CB46E2342B782FE7D04D244083DFC6A0443CA1945D&hospital_code=25039';
+  const url = 'https://moph-rbac.id.th/api/v1/token?Action=get_moph_access_token&user=Mallika.dev&password_hash=FC4BFDB41281744F341CBFB017F247DC9199C3EA877C3CD2D065D386029A8C38&hospital_code=25039';
 
   const res = http.post(url);
 
